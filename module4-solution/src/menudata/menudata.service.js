0afd8b76091955e06ec8bb58c6bcc4b93a0e8855
DmitriyKrupskiy <dsk_petr@mail.ru>
2019-04-14 21:36:58 +0300

    fix

--- module4-solution/src/menudata/menudata.service.js
+++ module4-solution/src/menudata/menudata.service.js
@@ -3,7 +3,7 @@
 
 angular.module('MenuData')
 .service('MenuDataService', MenuDataService)
-.constant('ApiBasePath', "http://davids-restaurant.herokuapp.com");
+.constant('ApiBasePath', "https://davids-restaurant.herokuapp.com");
 
 MenuDataService.$inject = ['$q', '$http', 'ApiBasePath']
 function MenuDataService($q, $http, ApiBasePath) {
